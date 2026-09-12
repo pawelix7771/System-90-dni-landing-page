5bef9ba0c0dd530eeec30011b0f512b342edd8c3
pawelix7771 <pawelix.7771@gmail.com>
2026-09-12 13:37:53 +0200

    Update create-checkout.ts

--- src/handlers/create-checkout.ts
+++ src/handlers/create-checkout.ts
@@ -46,7 +46,7 @@ export async function handleCreateCheckout(
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-      automatic_payment_methods: { enabled: true },
+      automatic_payment_methods: { "blik", "card" },
       line_items: [
         {
           price_data: {
